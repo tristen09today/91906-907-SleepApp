5300626cadf9a15ab4b6eb7d50c6574f1baa3197
Tristen Shieh <tristenshieh09@gmail.com>
2026-08-03 12:06:07 +1200

    Importing modules

--- Version1.py
+++ Version1.py
@@ -1,12 +1,6 @@
-#This program is to help track sleep when sleeping and when awake login systme Version 1
-
+#This is the Version 1 of the code for my Sleep App. 
+#This will be a basic version implmenting complex techniques like Json, Tkinter, Hashing, Encryption.
 
 from tkinter import *
-
-
-
-
-
-
-
-
+import json
+import hashlib
